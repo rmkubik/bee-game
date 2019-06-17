0a8408be17d5382a690a896b537eaf754742642c
Ryan Kubik <rmkubik@me.com>
2019-06-16 20:50:43 -0700

    A lot of boilerplate... and more

---
+++ src/index.js
@@ -0,0 +1,108 @@
+import {
+  init,
+  Sprite,
+  SpriteSheet,
+  GameLoop,
+  initKeys,
+  keyPressed,
+  TileEngine
+} from "kontra";
+import sprites from "../raw/images/sprites.png";
+import "./styles/index.css";
+
+const { canvas } = init("game");
+
+initKeys();
+
+let image = new Image();
+image.src = sprites;
+
+let bg;
+let sprite;
+
+image.onload = () => {
+  const spriteSheet = SpriteSheet({
+    image,
+    frameWidth: 8,
+    frameHeight: 8,
+    animations: {
+      bee: {
+        frames: 34
+      }
+    }
+  });
+
+  bg = Sprite({
+    x: 0,
+    y: 0,
+    width: 1000,
+    height: 1000,
+    color: "#008751"
+  });
+
+  sprite = Sprite({
+    x: 100, // starting x,y position of the sprite
+    y: 80,
+    // color: "red", // fill color of the sprite rectangle
+    animations: spriteSheet.animations,
+    // width: 20, // width and height of the sprite rectangle
+    // height: 40,
+    update: () => {
+      if (keyPressed("a")) {
+        sprite.x += -2;
+      } else if (keyPressed("d")) {
+        sprite.x += 2;
+      }
+
+      if (keyPressed("w")) {
+        sprite.y += -2;
+      } else if (keyPressed("s")) {
+        sprite.y += 2;
+      }
+    }
+  });
+
+  let tileEngine = TileEngine({
+    tilewidth: 8,
+    tileheight: 8,
+    width: 3,
+    height: 3,
+    tilesets: [
+      {
+        firstgid: 0,
+        image
+      }
+    ],
+
+    // layer object
+    layers: [
+      {
+        name: "ground",
+        data: [5, 5, 5, 5, 5, 5, 5, 5, 5]
+      }
+    ]
+  });
+
+  sprite.playAnimation("bee");
+
+  let loop = GameLoop({
+    // create the main game loop
+    update: function() {
+      // update the game state
+      sprite.update();
+
+      // wrap the sprites position when it reaches
+      // the edge of the screen
+      if (sprite.x > canvas.width) {
+        sprite.x = -sprite.width;
+      }
+    },
+    render: () => {
+      bg.render();
+      tileEngine.render();
+      sprite.render();
+    }
+  });
+
+  loop.start(); // start the game
+};
